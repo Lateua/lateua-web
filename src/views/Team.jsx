99393f5f5dd6728a1b741f6a092cc4d90b8008c3
unknown <rakelms90@gmail.com>
2022-11-02 09:36:01 +0100

    team page done

--- src/views/Team.jsx
+++ src/views/Team.jsx
@@ -1,9 +1,29 @@
+import person1 from '../img/iria.jpg';
+import person2 from '../img/nia.jpg';
+import person3 from '../img/rob.jpg';
+import person4 from '../img/rak.jpg';
 
+import '../styles/Team.css';
 
 export const Team = () => {
     return (
-        <div>
-            <h1>Team</h1>
+        <div className="teamContainer">
+            <div className="person">
+                <img src={person1} alt="person1" className="person1Img"/>
+                <p>Iria Sanz <br/> CTO</p>
+            </div>
+            <div className="person">
+                <img src={person2} alt="person2" className="person2Img"/>
+                <p>Estefanía Ferrer  <br/> Diseño 3D</p>
+            </div>
+            <div className="person">
+                <img src={person3} alt="person3" className="person3Img"/>
+                <p>Roberto Gonzalez<br/> Diseño 3D</p>
+            </div>
+            <div className="person">
+                <img src={person4} alt="person4" className="person4Img"/>
+                <p>Raquel Moya <br/> Fullstack Developer</p>
+            </div>
         </div>
     );
 };
